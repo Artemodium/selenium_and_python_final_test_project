4bf043873beed3badfe46415ad75296e5213f739
Судаков Артем Григорьевич <sudakov_ag@3pol-313a.uzo.nov>
2021-01-19 15:04:27 +0500

    Edited locators.py, added LoginLageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,11 @@
 from selenium.webdriver.common.by import By
-
+from .base_page import BasePage
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators(BasePage):
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
+    LOGIN = "login"
