c9dee5709af30a1b91982b4290b6a9c88d98a82b
Судаков Артем Григорьевич <sudakov_ag@3pol-313a.uzo.nov>
2021-01-19 11:04:11 +0500

    shuold_be_login added to main_page.MainPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,3 +10,8 @@ class MainPage(BasePage):
         time.sleep(5)
         login_link.click()
         time.sleep(4)
+
+    def should_be_login_link(self):
+        time.sleep(5)
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        time.sleep(3)
