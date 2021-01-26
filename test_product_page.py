96d1c226065363ddf95b861c64ffcf11c705af39
Судаков Артем Григорьевич <sudakov_ag@uzo.nov>
2021-01-26 15:49:40 +0500

    Addeed 'setup' - function and class

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,37 @@
-from .pages.main_page import MainPage
-from .pages.product_page import ProductPage
-from .pages.basketpage import BasketPage
 import pytest
+import time
+from .pages.basketpage import BasketPage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+
+
+@pytest.mark.test_class
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = "pa$$w0rd!"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.add_to_basket()
+        product_page.solve_quiz_and_get_code()
+        product_page.should_be_product_page()
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -18,43 +48,38 @@ import pytest
 def test_guest_can_add_product_to_basket(browser, link):
     #   link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     #   link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_basket_link()
-    page.go_to_product_page()
-    page.solve_quiz_and_get_code()
-    product_page = ProductPage(browser, browser.current_url)
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
     product_page.should_be_product_page()
 
 
 @pytest.mark.test
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_product_page()
-    page.solve_quiz_and_get_code()
-    product_page = ProductPage(browser, browser.current_url)
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
     product_page.should_not_be_success_message()
 
 
 @pytest.mark.test
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = MainPage(browser, link)
-    page.open()
-    product_page = ProductPage(browser, browser.current_url)
+    product_page = ProductPage(browser, link)
+    product_page.open()
     product_page.should_not_be_success_message()
 
 
 @pytest.mark.test
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_product_page()
-    page.solve_quiz_and_get_code()
-    product_page = ProductPage(browser, browser.current_url)
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
     product_page.should_disappeared_success_message()
 
 
@@ -83,8 +108,3 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_product_in_basket()
     basket_page.should_be_message_no_product_in_basket()
-
-
-
-
-
