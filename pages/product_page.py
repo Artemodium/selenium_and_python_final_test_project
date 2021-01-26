11ee47b59bf0d9d6f35fa1616912e90b7624a272
Судаков Артем Григорьевич <sudakov_ag@uzo.nov>
2021-01-26 15:51:51 +0500

    Added marks

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,13 +1,16 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 from selenium.common.exceptions import NoSuchElementException
-import time
 
 
 class ProductPage(BasePage):
     def should_be_product_page(self):
         self.should_be_message_product_in_basket_valid()
         self.should_be_product_price_valid()
+
+    def add_to_basket(self):
+        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_LINK)
+        link.click()
 
     def should_be_message_product_in_basket_valid(self):
         try:
@@ -26,3 +29,14 @@ class ProductPage(BasePage):
         except NoSuchElementException:
             assert False
         assert True
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGES_AFTER_ADD_TO_BASKET), \
+                "Success message is presented, but should not be"
+
+    def should_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGES_AFTER_ADD_TO_BASKET), \
+                "Success message is not disappeared, but should be"
+
+
+
