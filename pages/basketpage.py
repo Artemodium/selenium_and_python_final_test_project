46038efd6babca8a769af1aa0fc960ed42d84445
Судаков Артем Григорьевич <sudakov_ag@uzo.nov>
2021-01-26 15:55:25 +0500

    modified

--- pages/basketpage.py
+++ pages/basketpage.py
@@ -8,4 +8,4 @@ class BasketPage(BasePage):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_SUMMARY), "Basket is not empty!"
 
     def should_be_message_no_product_in_basket(self):
-        assert self.is_element_present(*BasketPageLocators.MESSAGE_EMPTY_BASKET)
+        assert self.is_element_present(*BasketPageLocators.MESSAGE_EMPTY_BASKET), "No awaiting message!"
